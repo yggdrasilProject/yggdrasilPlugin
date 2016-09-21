af5ed41dbfe4b107c42d50ec8d9134df25fb8244
Alexey Deryugin <aderyugin@mirantis.com>
2016-09-21 15:27:59 +0300

    Initial implementation

---
+++ src/main/resources/archetype-resources/src/main/java/ExampleService.java
@@ -0,0 +1,22 @@
+package $ru.linachan;
+
+import ru.linachan.yggdrasil.service.YggdrasilService;
+
+public class ExampleService implements YggdrasilService {
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onShutdown() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
